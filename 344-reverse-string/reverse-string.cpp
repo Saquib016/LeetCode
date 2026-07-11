37ea64d6800cc4bdfc0aaa6a56809a490572564e
Saquib Salim Kidwai <2k23.aids2312702@gmail.com>
2026-07-11 20:11:51 +0530

    Time: 73 ms (2.03%) | Memory: 27.3 MB (52.73%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -3,7 +3,9 @@ public:
     void reverseString(vector<char>& s) {
         int n=s.size();
         for (int i=0;i<n/2;i++){
-            swap(s[i],s[n-i-1]);
+            int temp = s[i];
+            s[i]= s[n-i-1];
+            s[n-i-1] = temp;
         }
         for(int i=0;i<n;i++){
             cout<<s[i];
