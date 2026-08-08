3c150731ce913ead9cc3046e6aa6f56dc705eaf4
Saquib Salim Kidwai <2k23.aids2312702@gmail.com>
2026-08-08 20:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (15.18%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,10 +3,11 @@ public:
     int maxSubArray(vector<int>& nums) {
         int curr = nums[0];
         int best = nums[0];
-        for(int i=1;i<nums.size();i++){
-            curr = max(curr+nums[i],nums[i]);
+        for(int i = 1;i<nums.size();i++){
+            curr = max(nums[i]+curr,nums[i]);
             best = max(curr,best);
         }
         return best;
+
     }
 };
