d665ae47780e8e36d0b3ed3259dc7a8773deb03a
Saquib Salim Kidwai <2k23.aids2312702@gmail.com>
2026-08-22 10:43:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.17%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool checkDivisibility(int n) {
-        int sum = 0;
-        int prd = 1;
-        int temp = n;
-        while (n>0){
-            int dig = n%10;
-            sum+=dig;
-            prd*=dig;
-            n = n/10;
+    bool checkDivisibility(int y) {
+        int n=y;
+        int prod=1,sum=0;
+        int x;
+        while(n!=0)
+        {
+            x=n%10;
+            sum+=x;
+            prod*=x;
+            n/=10;
         }
-        cout<<sum<<endl<<prd;
-        if(temp%(sum+prd)==0) return true;
-        else return false;
+        if(y%(sum+prod)==0) return true;
+        return false;
     }
 };
